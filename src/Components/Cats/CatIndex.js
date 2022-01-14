176c30c48f8cdf9d5bf0fbaef040b2ce0131c3e4
Danielle Joy <daniellejoy@danielles-mbp.lan>
2022-01-13 21:18:21 -0500

    cats

--- src/Components/Cats/CatIndex.js
+++ src/Components/Cats/CatIndex.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import CatList from './CatList';
+// import CatList from './CatList';
  
-class CatIndex {
- constructor(){
-   super(props)
-   state : {
+export default class CatIndex extends Component {
+ constructor(props){
+   super(props);
+   this.state = {
      breeds: ['persian', 'siamese', 'maine coon', 'ragdoll', 'scottish fold', 'sphynx', 'british shorthair', 'bengal', 'american shorthair']
    }
  }
-
+render (){
    return (
-     <div>
-      <CatList cats={breeds} />
-     </div>
-   );
+           <div>
+           <li>
+      <CatList breeds={this.state.breeds} />
+      </li>
+      </div>)
+       }
 
+}
+
+
+class CatList extends Component {
+  render(){
+    return(
+      <div>
+        {this.props.breeds.map((cat) => {
+          return <Cats passingCats={cat}/>;
+        })}
+      </div>
+    );
+  }
+}
+
+const Cats = (props) => {
+  return (
+    <div>
+      <h3>{props.passingCats}</h3>
+    </div>
+  )
 }
 
--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
  
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
-}
- 
-CatList;
+// export default class CatList extends Component {
+//   render (){
+//  return (
+//    <div>
+//      {props.breeds.Map(cat => <li>cat</li> )}
+//    </div>
+//  )
+// }
+// }
